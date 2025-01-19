c5932d39cb2c46d3444f713623e6b6ef18c98ee9
RisAhamed <riswanahamed38@gmail.com>
2025-01-19 20:03:15 +0530

    Cicd Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'end to end chatbot',
+    version= '0.0.0',
+    author= 'Riswan Ahamed',
+    author_email= 'riswanahamed38@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
